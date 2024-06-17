27a404d70a2f799b80c5e5858b24ae0889cbf1b7
cryptolisboa <dey00tsnetwork@gmail.com>
2024-06-17 23:02:08 +0100

    + debug auth redirect

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,10 +3,19 @@ import { createClient } from "@/utils/supabase/client";
 export const signInDirect = async () => {
   const supabase = createClient();
 
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  if (!baseUrl) {
+    throw new Error("NEXT_PUBLIC_BASE_URL is not set");
+  } else {
+    console.log("baseUrl: ", baseUrl);
+    debugger;
+  }
+
   await supabase.auth.signInWithOAuth({
     provider: "discord",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`,
+      redirectTo: `${baseUrl}/api/auth/callback`,
     },
   });
 };
