061c317611fb6ed40e83bb4b41ee38436cb78329
cryptolisboa <dey00tsnetwork@gmail.com>
2024-06-17 21:05:33 +0100

    + add app metadata + add monad-builders image on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,39 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Monad Builders",
-    description: "Monad Builders is an explorer powered by the House of Molandak to discover everything Monad including, NFTs, games, DeFi and more",
+  title: "Monad Builders",
+  description:
+    "Monad Builders is a community built explorer to showcase and discover everything Monad including, NFTs, games, DeFi and more",
+  applicationName: "Monad Builders",
+  keywords: [
+    "Monad",
+    "Monad Builders",
+    "Monad Explorer",
+    "Monad NFTs",
+    "Monad Games",
+    "Monad DeFi",
+    "Monad Community",
+  ],
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <NextUIProvider>{children}</NextUIProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="icon" href="/images/monad-builders.jpeg" type="image/jpeg" />
+      </Head>
+      <body className={inter.className}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
 import { fetchPublicSheet } from "@/services/public-sheet-fetcher";
 import ProjectList from "./components/ProjectList";
+import Image from "next/image";
 
 export default async function Home() {
-    const data = await fetchPublicSheet();
-    console.log(JSON.stringify(data, null, 2));
-    return (
-        <main className="bg-purple-900 min-h-screen p-4">
-            <div className="text-white">
-                <h1 className="text-2xl lg:text-4xl text-center font-bold">Monad Projects</h1>
-                <ProjectList className="mt-4 lg:mt-8" data={data} />
-            </div>
-        </main>
-    );
+  const data = await fetchPublicSheet();
+  console.log(JSON.stringify(data, null, 2));
+  return (
+    <main className="bg-purple-900 min-h-screen p-4">
+      <div className="text-white">
+        <h1 className="text-2xl lg:text-4xl text-center font-bold">
+          Monad Projects
+        </h1>
+        <div className="flex justify-center items-center mt-4">
+          <Image
+            src="/images/monad-builders.jpeg"
+            alt="Monad Builders"
+            width={100}
+            height={100}
+            unoptimized
+            className="w-24 h-24 md:w-48 md:h-48" // Tailwind classes for responsive sizes
+          />
+        </div>
+        <ProjectList className="mt-4 lg:mt-8" data={data} />
+      </div>
+    </main>
+  );
 }
