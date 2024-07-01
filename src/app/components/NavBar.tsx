6c896ef325f53cc75946b7456cd92be0d84a2c0b
cryptolisboa <dey00tsnetwork@gmail.com>
2024-07-01 20:08:56 +0100

    refactor: update NavBar styling and fonts

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className="bg-gradient-to-r from-gradient-start to-gradient-end flex justify-center items-center h-14 md:h-16 px-4 md:px-8">
+    <div className="bg-gradient-to-r from-gradient-start to-gradient-end flex justify-center items-center h-14 md:h-16 px-4 md:px-8 opacity-80">
       <Link href="/">
-        <h1 className="text-2xl lg:text-4xl text-center font-bold text-white">
+        <h1 className="text-2xl lg:text-4xl text-center text-white">
           {/* Monad Builders */}
-          <span className="font-roboto">{"Monad "}</span>
+          <span className="font-roboto font-semibold">{"Monad "}</span>
           <span className="font-lobster">Builders</span>
         </h1>
       </Link>
